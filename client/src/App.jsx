111665ae13c92e07e6fb84429ee68ed4055f4f72
Pratiksha Bhattacharyya <prtksha@gmail.com>
2026-03-06 01:04:51 +0530

    TeaSphere AI translation + dashboard update

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import axios from "axios";
 
-const API_BASE = "http://localhost:3001";
+const API_BASE = "https://tea-sphere-ai.onrender.com";
 
 export default function App() {
 
